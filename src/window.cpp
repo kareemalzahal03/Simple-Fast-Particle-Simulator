447806373663f800c0c8c3fb43b879cadbf59244
Kareem Alzahal <kalzahal@icloud.com>
2024-08-20 15:04:10 -0400

    add filesystem

--- src/window.cpp
+++ src/window.cpp
@@ -1,8 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include "window.hpp"
+#include <filesystem>
+
+static void modifyCurrentWorkingDirectory() {
+    // For cross platform use
+    while (!std::filesystem::exists("resources"))
+    {
+        std::filesystem::current_path(std::filesystem::current_path().parent_path());
+    }
+    auto cwd = std::filesystem::current_path();
+}
 
 Window::Window(const sf::String& title) 
 : sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), title, sf::Style::Close) {
+
+    modifyCurrentWorkingDirectory();
 
     fpsTextFont.loadFromFile("resources/FiraCode-Regular.ttf");
     fpsText.setFont(fpsTextFont);
